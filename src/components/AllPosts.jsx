9e51c0d24e6968ebe9a74c735bc49b83b8e89282
Will Moran <will.c.moran@gmail.com>
2024-05-30 15:13:12 -0400

    Added styling for posts

--- src/components/AllPosts.jsx
+++ src/components/AllPosts.jsx
@@ -1,5 +1,7 @@
+import "./AllPosts.css"
 import { useEffect, useState } from "react"
 import { getAllPosts } from "../services/postsService.jsx"
+import { Card } from "react-bootstrap"
 
 export const AllPosts = () => {
     const [allPosts, setAllPosts] = useState([])
@@ -16,16 +18,22 @@ export const AllPosts = () => {
 
 
     return (
-        <section>
-            {allPosts.map((post) => {
-                return (
-                    <div key={post.id}>
-                        <div>Title: {post.title}</div>
-                        <div>Topic: {post.topic.name}</div>
-                        <div>Likes: {post.userLikes.length}</div>
-                    </div>
-                )
-            })}
-        </section>
+        <body className="pageBody">
+            <section className="postBody">
+                {allPosts.map((post) => {
+                    return (
+                        <Card className="postCard" key={post.id}>
+                            <Card.Title className="postCardTitle">Title: {post.title}</Card.Title>
+                            <Card body>
+                                <div className="postCardBody">
+                                    <div>Topic: {post.topic.name}</div>
+                                    <div>Likes: {post.userLikes.length}</div>
+                                </div>
+                            </Card>
+                        </Card>
+                    )
+                })}
+            </section>
+        </body>
     )
 }
